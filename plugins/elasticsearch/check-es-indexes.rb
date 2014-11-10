830c5f5f2d12b6e862ca14cf916d415a2d9618bd
Anthony Spring <tony@porkchopsandpaintchips.com>
2014-11-10 09:39:35 -0500

    Adding ignore option to check. Adding shortcut if only a single server is given.

--- plugins/elasticsearch/check-es-indexes.rb
+++ plugins/elasticsearch/check-es-indexes.rb
@@ -10,12 +10,21 @@ class CheckESClusterIndex < Sensu::Plugin::Check::CLI
     :long => '--cluster CLUSTER[,CLUSTER]',
     :proc => proc { |a| a.split(',') }
 
+  option :ignore,
+    :description => 'Comma separated list of indexes to ignore',
+    :short => '-i INDEX[,INDEX]',
+    :long => '--ignore INDEX[,INDEX]',
+    :proc => proc { |a| a.split(',') }
+
   option :debug,
     :description => 'Debug',
     :short => '-d',
     :long => '--debug'
 
   def run
+    # If only one cluster is given, no need to check the indexes 
+    ok 'All indexes are unique' if config[:cluster].length == 1
+
     port = ':9200'
     cmd = '/_cat/indices?v | tail -n +2'
 
@@ -30,6 +39,10 @@ class CheckESClusterIndex < Sensu::Plugin::Check::CLI
       index_arr = `curl -s #{ u }#{ port }#{ cmd }`.split("\n")
       index_arr.each do |t|
         t = t.split[1]
+
+        # If the index is in the ignore list, go to the next one
+        next if config[:ignore].include? t
+
         if valid_index.key?(t)
           dupe_index[t] = [] unless dupe_index[t].is_a?(Array)
           dupe_index[t] << u

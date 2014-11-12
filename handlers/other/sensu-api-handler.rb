1b622ac4ca5af3534c5ec86638cb35534805f20c
Matt Jones <mjones@yieldbot.com>
2014-11-11 21:19:53 -0500

    Add server-side api script, not functional

--- handlers/other/sensu-api-handler.rb
+++ handlers/other/sensu-api-handler.rb
@@ -2,7 +2,7 @@
   "handlers": {
     "sensu-api-handler": {
       "type": "pipe",
-      "command": "/etc/sensu/handlers/sensu-yieldbot-plugins/sensu-api-handler.rb"
+      "command": "/etc/sensu/handlers/sensu-yieldbot-plugins/sensu-handler.rb"
     }
   }
 }

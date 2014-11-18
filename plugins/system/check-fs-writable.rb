447dc11ffb207ac859575cbbd34f0991310f7ee5
Matt Jones <mjones@yieldbot.com>
2014-11-18 10:07:20 -0500

    Refactor codebase

--- plugins/system/check-fs-writable.rb
+++ plugins/system/check-fs-writable.rb
@@ -33,11 +33,6 @@ require 'rubygems' if RUBY_VERSION < '1.9.0'
 require 'sensu-plugin/check/cli'
 require 'tempfile'
 
-# Exit status codes
-$EXIT_OK = 0
-$EXIT_WARNING = 1
-$EXIT_CRIT = 2
-
 class CheckFSWritable < Sensu::Plugin::Check::CLI
 
   # #YELLOW
@@ -45,33 +40,30 @@ class CheckFSWritable < Sensu::Plugin::Check::CLI
   #   for each directory but one check for as many directories as desired
   option :dir,
          :description => 'Directory to check for writability',
-         :short => '-d DIRECTORY',
-         :long => '--directory DIRECTORY',
-         :proc => proc { |a| a.split(',') }
+         :short       => '-d DIRECTORY',
+         :long        => '--directory DIRECTORY',
+         :proc        => proc { |a| a.split(',') }
 
   option :auto,
          :description => 'Auto discover mount points via fstab',
-         :short => '-a',
-         :long => '--auto-discover'
+         :short       => '-a',
+         :long        => '--auto-discover'
 
   option :debug,
          :description => 'Print debug statements',
-         :long => '--debug'
+         :long        => '--debug'
 
   def initialize
     super
-    # @@exit_code = $EXIT_OK
-    @@crit_pt_proc = []
-    @@crit_pt_test = []
+    @crit_pt_proc = []
+    @crit_pt_test = []
   end
 
   def usage_summary
-    if @@crit_pt_test.empty? && @@crit_pt_proc.empty?
-      puts 'All filesystems are writable'
-      exit($EXIT_OK)
-    elsif @@crit_pt_test || @@crit_pt_proc
-      puts "The following file systems are not writeable: #{ @@crit_pt_test }, #{@@crit_pt_proc}"
-      exit($EXIT_CRIT)
+    if @crit_pt_test.empty? && @crit_pt_proc.empty?
+      ok 'All filesystems are writable'
+    elsif @crit_pt_test || @crit_pt_proc
+      critical "The following file systems are not writeable: #{ @crit_pt_test }, #{@crit_pt_proc}"
     end
   end
 
@@ -82,19 +74,19 @@ class CheckFSWritable < Sensu::Plugin::Check::CLI
 
   def is_rw_in_proc(mount_info)
     mount_info.each  do |pt|
-      @@crit_pt_proc <<  "#{ pt.split[0] }" if pt.split[1] != 'rw'
+      @crit_pt_proc <<  "#{ pt.split[0] }" if pt.split[1] != 'rw'
     end
   end
 
   def is_rw_test(mount_info)
     mount_info.each do |pt|
-    Dir.exist? pt.split[0] || @@crit_pt_test << "#{ pt.split[0] }"
+    Dir.exist? pt.split[0] || @crit_pt_test << "#{ pt.split[0] }"
     file = Tempfile.new('.sensu', pt.split[0])
     puts "The temp file we are writing to is: #{ file.path }" if config[:debug]
     # #YELLOW
     #  need to add a check here to validate permissions, if none it pukes
-    file.write('mops') || @@crit_pt_test <<  "#{ pt.split[0] }"
-    file.read || @@crit_pt_test <<  "#{ pt.split[0] }"
+    file.write('mops') || @crit_pt_test <<  "#{ pt.split[0] }"
+    file.read || @crit_pt_test <<  "#{ pt.split[0] }"
     file.close
     file.unlink
     end
@@ -106,30 +98,28 @@ class CheckFSWritable < Sensu::Plugin::Check::CLI
     if config[:auto]
       # #YELLOW
       # this will only work for a single namespace as of now
-      mount_info = Array.new
       mount_info = get_mnt_pts.split("\n")
+      warning 'No mount points found' if mount_info.length == 0
       # #YELLOW
       #  I want to map this at some point to make it pretty and eaiser to read for large filesystems
       puts 'This is a list of mount_pts and their current status: ', mount_info if config[:debug]
       is_rw_in_proc(mount_info)
       is_rw_test(mount_info)
-      puts "The critical mount points according to proc are: #{ @@crit_pt_proc }" if config[:debug]
-      puts "The critical mount points according to actual testing are: #{ @@crit_pt_test }" if config[:debug]
+      puts "The critical mount points according to proc are: #{ @crit_pt_proc }" if config[:debug]
+      puts "The critical mount points according to actual testing are: #{ @crit_pt_test }" if config[:debug]
     elsif config[:dir]
       puts config[:dir]
 
-      Dir.exist? config[:dir] || @@crit_pt_test << "#{ config[:dir] }"
+      Dir.exist? config[:dir] || @crit_pt_test << "#{ config[:dir] }"
       file = Tempfile.new('.sensu', config[:dir])
       puts "The temp file we are writing to is: #{ file.path }" if config[:debug]
       # #YELLOW
       #  need to add a check here to validate permissions, if none it pukes
-      file.write('mops') || @@crit_pt_test <<  "#{ config[:dir] }"
-      file.read || @@crit_pt_test <<  "#{ config[:dir] }"
+      file.write('mops') || @crit_pt_test <<  "#{ config[:dir] }"
+      file.read || @crit_pt_test <<  "#{ config[:dir] }"
       file.close
       file.unlink
     end
-    usage_summary # unless @crit_fs.empty?
-    # usage_summary unless @warn_fs.empty?
-    # exit(@exit_code)
+    usage_summary
   end
 end

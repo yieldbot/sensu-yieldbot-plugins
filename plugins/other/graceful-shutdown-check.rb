34513e422e8d3864a064a5a8946d0583298aa520
Matt Jones <mattyjones@users.noreply.github.com>
2014-12-12 11:20:37 -0500

    Rubocop and doc changes

--- plugins/other/graceful-shutdown-check.rb
+++ plugins/other/graceful-shutdown-check.rb
@@ -9,7 +9,7 @@
 #   to reduce the chance of a keepalive handler firing for them.
 #
 # PLATFORMS:
-#   all
+#   Linux
 #
 # DEPENDENCIES:
 #   gem: sensu-plugin
@@ -33,10 +33,21 @@ require 'sensu-plugin/check/cli'
 require 'sensu-plugin/utils'
 require 'json'
 
+#
+# == Graceful Shutdown Check
+#
 class GracefulShutdownCheck < Sensu::Plugin::Check::CLI
   include Sensu::Plugin::Utils
 
-  def api_request(method, path, &blk)
+  # Make an api request to the sensu api
+  #
+  # ==== Attributes
+  #
+  # * +method+ - the type of request: POST, GET, etc
+  # * +path+ - the path of the api request
+  # * +&blk+ - ?
+  #
+  def api_request(method, path, &blk) # rubocop:disable all
     http = Net::HTTP.new(settings['api']['host'], settings['api']['port'])
     req = net_http_req_class(method).new(path)
     if settings['api']['user'] && settings['api']['password']
@@ -46,33 +57,44 @@ class GracefulShutdownCheck < Sensu::Plugin::Check::CLI
     http.request(req)
   end
 
+  # This will delete the Sensu client if requested
+  #
+  # ==== Attributes
+  #
+  # * +client+ - the client to remove from the dashboard
+  #
   def delete_sensu_client!(client)
     api_request(:DELETE, "/clients/#{client}")
   end
 
-  def graceful_clients
+  # Get the stashes and check to see if they have a graceful shutdown stash
+  # associated with them
+  #
+  def graceful_clients # rubocop:disable Metrics/MethodLength
     keyspace = settings['graceful-shutdown']['keyspace']
 
     # Get a list of the stashes
     response = api_request(:GET, '/stashes')
 
-    # Make sure we are able to retrieve the stathses
+    # Make sure we are able to retrieve the stashes
     critical 'Unable to retrieve stashes' if response.code != '200'
-    
+
     #
     all_stashes = JSON.parse(response.body)
 
     # Filter the stathes
     filtered_stashes = []
     all_stashes.each do |stash|
-      if match = stash['path'].match(/^#{keyspace}\/(.*)/)
-        filtered_stashes << match.captures[0]  
+      if match = stash['path'].match(/^#{keyspace}\/(.*)/) # rubocop:disable all
+        filtered_stashes << match.captures[0]
       end
     end
 
     filtered_stashes
   end
 
+  # If a client has a graceful shutdown stash then remove it
+  #
   def run
     graceful_clients.each do |client|
       delete_sensu_client!(client)

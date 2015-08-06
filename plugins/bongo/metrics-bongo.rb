44af65baea7e81ab08415240ac30b808b6bb1929
Matt Jones <mattjones@yieldbot.com>
2015-08-06 12:33:18 -0400

    adjust formating of metrics

--- plugins/bongo/metrics-bongo.rb
+++ plugins/bongo/metrics-bongo.rb
@@ -33,7 +33,7 @@ require 'rest-client'
 #
 # Get a set of metrics from an app running in Mesos
 #
-class MesosAppMetrics < Sensu::Plugin::Metric::CLI::Graphite
+class MesosAppMetrics < Sensu::Plugin::Metric::CLI::Statsd
   option :server,
          description: 'The consul dns name of the mesos server',
          short: '-s SERVER',
@@ -52,24 +52,14 @@ class MesosAppMetrics < Sensu::Plugin::Metric::CLI::Graphite
   # Acquire the slave that a particular app is running on
   #
   def acquire_app_slave
-    # the consul dns name of the server to get the app's current slave
-    #
+    # consul dns should always be used
     server = config[:server] || 'us-east-1-perpetuum.mesos-marathon.service.consul'
-
-    # default port to get hit for the info
     # the default port is the one supplied by consul
-    #
     port   = config[:port] || '443'
-
-    # the name of the app you wish to reterive metrics on
-    #
-    app    = config[:app] || # store failuers here
-             #
-             @failures = []
+    app    = config[:app]
+    @failures = []
 
     # break out if the client fails to connect to the mesos master
-    # this will not error on returned data just on socket issues
-    #
     begin
         r = RestClient::Resource.new("https://#{server}:#{port}/v2/apps/#{app}", timeout: 10, verify_ssl: false).get
         if r.code != 200
@@ -83,7 +73,6 @@ class MesosAppMetrics < Sensu::Plugin::Metric::CLI::Graphite
     JSON.parse(r)['app']['tasks'][0]['host']
   end
 
-  #
   # reterive the metrics from the app
   #
   def acquire_metrics(current_slave)
@@ -93,7 +82,9 @@ class MesosAppMetrics < Sensu::Plugin::Metric::CLI::Graphite
   def run
     current_slave = acquire_app_slave
     critical @failures unless @failures.nil?
-    acquire_metrics(current_slave)
+    acquire_metrics(current_slave).each do |k,v|
+      output [k,v].join('.')
+    end
     ok
   end
 end

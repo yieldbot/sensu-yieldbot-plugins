4e1db599d0230a017cd695bcfaa385b33877981d
Pratik <pnarode@yieldbot.com>
2016-01-23 03:04:24 +0530

    use exists api instead of cat api to check if index exists or not

--- plugins/elasticsearch/check-es-tribe-index.py
+++ plugins/elasticsearch/check-es-tribe-index.py
@@ -19,75 +19,70 @@ OUTPUT = []
 myname = socket.gethostname()
 
 def check_aggregation_cluster(cluster):
-    indarr = []
     now = datetime.datetime.utcnow()
+    conn = httplib.HTTPConnection(cluster)
+    missing = ""
+    indices = []
     for i in range(2):
         dt = datetime.datetime(year=now.year, month=now.month, day=now.day) - datetime.timedelta(days=(i*30))
         dt_str = dt.strftime("%Y-%m")
-        indarr.append("aggstats-" + dt_str)
-    conn = httplib.HTTPConnection(cluster)
-    conn.request("GET", "/_cat/indices")
-    r1 = conn.getresponse()
-    if r1.status >= 300:  # pylint: disable=E1101
-        OUTPUT.append("check_aggregation_cluster: host=%s received non-2xx resp=%s"%(myname, r1.status))
-        r1.raise_for_status()
-    else:
-        rarr = ((r1.read()).split("\n"))
-        missing = ""
-        for ind in indarr:
-            if not any (ind in r for r in rarr):
-                missing = missing + "%s," % (ind)
-        if missing != "":
-            OUTPUT.append("check_aggregation_cluster: Index %s is missing" % (missing))
+        indices.append("aggstats-" + dt_str)
+    for i in range(len(indices)):
+        conn.request("GET", "/" + indices[i])
+        res = conn.getresponse()
+        if res.status == 404:  # pylint: disable=E1101
+            missing = missing + "%s," % (indices[i])
+        elif res.status != 200:
+            missing = ""
+            OUTPUT.append("check_aggregation_cluster: host=%s received non-2xx resp=%s"%(myname, res.status))
+            break
+    if missing != "":
+        OUTPUT.append("check_aggregation_cluster: Index %s is missing" % (missing))
 
 
 def check_hotevents_cluster(cluster):
-    indarr = []
     now = datetime.datetime.utcnow()
+    conn = httplib.HTTPConnection(cluster)
+    missing = ""
+    indices = []
     for i in range(2):
         dt = datetime.datetime(year=now.year, month=now.month, day=now.day) - datetime.timedelta(days=i)
         dt_str = dt.strftime("%Y-%m-%d")
-        indarr.append("adevents-" + dt_str)
-        indarr.append("pubevents-" + dt_str)
-    conn = httplib.HTTPConnection(cluster)
-    conn.request("GET", "/_cat/indices")
-    r1 = conn.getresponse()
-    if r1.status >= 300:  # pylint: disable=E1101
-        OUTPUT.append("check_hotevents_cluster: host=%s received non-2xx resp=%s"%(myname, r1.status))
-        r1.raise_for_status()
-    else:
-        rarr = ((r1.read()).split("\n"))
-        missing = ""
-        for ind in indarr:
-            if not any (ind in r for r in rarr):
-                missing = missing + "%s," % (ind)
-        if missing != "":
-            OUTPUT.append("check_hotevents_cluster: Index %s is missing" % (missing))
+        indices.append("adevents-" + dt_str)
+        indices.append("pubevents-" + dt_str)
+    for i in range(len(indices)):
+        conn.request("GET", "/" + indices[i])
+        res = conn.getresponse()
+        if res.status == 404:  # pylint: disable=E1101
+            missing = missing + "%s," % (indices[i])
+        elif res.status != 200:
+            missing = ""
+            OUTPUT.append("check_hotevents_cluster: host=%s received non-2xx resp=%s"%(myname, res.status))
+            break
+    if missing != "":
+        OUTPUT.append("check_hotevents_cluster: Index %s is missing" % (missing))
 
 def check_coldevents_cluster(cluster):
-    indarr = []
     now = datetime.datetime.utcnow()
+    conn = httplib.HTTPConnection(cluster)
+    missing = ""
+    indices = []
     for i in range(2):
         dt = datetime.datetime(year=now.year, month=now.month, day=now.day) - datetime.timedelta(days=(7+i))
         dt_str = dt.strftime("%Y-%m-%d")
-        indarr.append("adevents-" + dt_str)
-        indarr.append("pubevents-" + dt_str)
-    conn = httplib.HTTPConnection(cluster)
-    conn.request("GET", "/_cat/indices")
-    r1 = conn.getresponse()
-    if r1.status >= 300:  # pylint: disable=E1101
-        OUTPUT.append("check_coldevents_cluster: host=%s received non-2xx resp=%s"%(myname, r1.status))
-        r1.raise_for_status()
-    else:
-        rarr = ((r1.read()).split("\n"))
-        missing = ""
-        for ind in indarr:
-            if not any (ind in r for r in rarr):
-                missing = missing + "%s," % (ind)
-        if missing != "":
-            OUTPUT.append("check_coldevents_cluster: Index %s is missing" % (missing))
-
-
+        indices.append("adevents-" + dt_str)
+        indices.append("pubevents-" + dt_str)
+    for i in range(len(indices)):
+        conn.request("GET", "/" + indices[i])
+        res = conn.getresponse()
+        if res.status == 404:  # pylint: disable=E1101
+            missing = missing + "%s," % (indices[i])
+        elif res.status != 200:
+            missing = ""
+            OUTPUT.append("check_coldevents_cluster: host=%s received non-2xx resp=%s"%(myname, res.status))
+            break
+    if missing != "":
+        OUTPUT.append("check_coldevents_cluster: Index %s is missing" % (missing))
 
 if __name__ == '__main__':
     OUTPUT = []

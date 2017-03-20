e12cb8f0f7634d536e5ec9681866a92e7464ad36
Sunita Kawane <skawane@yieldbot.com>
2017-03-20 15:45:48 +0530

    remove west adservers

--- plugins/bongo/check-adserver-mm-health.py
+++ plugins/bongo/check-adserver-mm-health.py
@@ -13,9 +13,7 @@ adservers = {}
 adservers["east"] = ["prd-useast-service-adserver-c-01.prd.yb0t.cc",
                      "prd-useast-service-adserver-d-01.prd.yb0t.cc",
                      "prd-useast-service-adserver-e-01.prd.yb0t.cc"]
-adservers["west"] = ["prd-uswest-service-adserver-a-01.prd.yb0t.cc",
-                     "prd-uswest-service-adserver-b-01.prd.yb0t.cc",
-                     "prd-uswest-service-adserver-c-01.prd.yb0t.cc"]
+adservers["west"] = []
 
 def get_bongo_host(server, app):
     try:
